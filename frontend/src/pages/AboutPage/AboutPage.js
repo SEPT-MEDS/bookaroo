78c9c4d0ce47fa8b75c7fb82b1498dde0200c251
Sefanur Erciyas s3842307 <s3842307@student.rmit.edu.au>
2021-08-19 19:04:26 +1000

    Add AboutPage text

--- frontend/src/pages/AboutPage/AboutPage.js
+++ frontend/src/pages/AboutPage/AboutPage.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const AboutPage = () => {
-  return <div>
-    about
+  return <div className = "aboutPage">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sodales 
+      risus sed tincidunt maximus. Donec ut sapien sollicitudin, consequat leo 
+      vitae, porta arcu. Morbi nec tincidunt ex, eu gravida sem. Curabitur 
+      vulputate, arcu sit amet ornare semper, ante purus placerat ante, sed 
+      pellentesque purus tellus id magna.
+    </p>
+    <br/>
+    <h3>Check out our socials below!</h3>
+    <p>Facebook, Twitter and Instagram here...</p>
   </div>
 }
 
