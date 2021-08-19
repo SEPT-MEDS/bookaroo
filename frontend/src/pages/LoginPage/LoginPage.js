a301d197a6ef119cb8951bed409c9fc84a7aa946
Giraugh <s3845382@student.rmit.edu.au>
2021-08-19 19:30:37 +1000

    Update form to use theme and improve layout

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
-import { Container, Form, InputContainer } from './loginPageStyle'
+import { Container, Form, InputContainer, Heading, FieldsContainer } from './loginPageStyle'
 
 const LoginPage = () => {
   const {register, handleSubmit, formState: { errors } } = useForm()
@@ -10,16 +10,19 @@ const LoginPage = () => {
 
   return <Container>
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <InputContainer>
-        <label>Email</label>
-        <input type='email' {...register('email', { required: true })} />
-        {errors.email && 'This field is required'}
-      </InputContainer>
-      <InputContainer>
-        <label>Password</label>
-        <input type='password' {...register('password', { required: true })} />
-        {errors.password && 'This field is required'}
-      </InputContainer>
+      <Heading>Login</Heading>
+      <FieldsContainer>
+        <InputContainer>
+          <label>Email</label>
+          <input type='email' {...register('email', { required: true })} />
+          {errors.email && 'This field is required'}
+        </InputContainer>
+        <InputContainer>
+          <label>Password</label>
+          <input type='password' {...register('password', { required: true })} />
+          {errors.password && 'This field is required'}
+        </InputContainer>
+      </FieldsContainer>
       <input type='submit' value='Login' />
     </Form>
   </Container>
