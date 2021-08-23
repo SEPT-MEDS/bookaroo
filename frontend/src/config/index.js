35ff1d80fbb28e6f728e6ddee35f0998bcff886a
Giraugh <s3845382@student.rmit.edu.au>
2021-08-23 17:31:21 +1000

    Update configured port for API gateway

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -3,7 +3,7 @@ const env = 'development'
 const config = {
   development: {
     baseURL: 'http://localhost:3000',
-    apiAddress: 'http://localhost:3001/api',
+    apiAddress: 'http://localhost:9191/api',
   }
 }
 
