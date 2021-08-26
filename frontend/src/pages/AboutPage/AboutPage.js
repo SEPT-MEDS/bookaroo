243f34895b734fe5f4ba7b15880ad4d0fba7faa1
Sefanur Erciyas s3842307 <s3842307@student.rmit.edu.au>
2021-08-26 18:20:51 +1000

    Add margins to about page

--- frontend/src/pages/AboutPage/AboutPage.js
+++ frontend/src/pages/AboutPage/AboutPage.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { AboutInfoContainer, ContactPointsContainer } from './aboutPageStyle'
+import { 
+  Container, 
+  AboutInfoContainer, 
+  ContactPointsContainer 
+} from './aboutPageStyle'
 import { ContactPoint } from '../../components'
 
 const AboutPage = () => {
-  return <div className = "aboutPage">
-    <AboutInfoContainer>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sodales 
-        risus sed tincidunt maximus. Donec ut sapien sollicitudin, consequat leo 
-        vitae, porta arcu. Morbi nec tincidunt ex, eu gravida sem. Curabitur 
-        vulputate, arcu sit amet ornare semper, ante purus placerat ante, sed 
-        pellentesque purus tellus id magna.
-      </p>
-    </AboutInfoContainer>
-    <h3>Check out our socials below!</h3>
-    <ContactPoints></ContactPoints>
-  </div>
+  return (
+    <Container>
+      <AboutInfoContainer>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sodales 
+          risus sed tincidunt maximus. Donec ut sapien sollicitudin, consequat leo 
+          vitae, porta arcu. Morbi nec tincidunt ex, eu gravida sem. Curabitur 
+          vulputate, arcu sit amet ornare semper, ante purus placerat ante, sed 
+          pellentesque purus tellus id magna.
+        </p>
+      </AboutInfoContainer>
+      <h3>Check out our socials below!</h3>
+      <ContactPoints></ContactPoints>
+    </Container>
+  )
 }
 
 const ContactPoints = () =>
--- frontend/src/pages/AboutPage/aboutPageStyle.js
+++ frontend/src/pages/AboutPage/aboutPageStyle.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
+export const Container = styled.div`
+  margin: 2em;
+  margin-left: 5em;
+  margin-right: 5em;
+  gap: 2em;
+`
+
 export const ContactPointsContainer = styled.div`
   grid-column: 2;
   grid-template-columns: 1fr;
@@ -7,5 +14,5 @@ export const ContactPointsContainer = styled.div`
   height: max-content;
 `
 export const AboutInfoContainer = styled.div`
-  margin-bottom: 20px;
+  margin-bottom: 2em;
 `
