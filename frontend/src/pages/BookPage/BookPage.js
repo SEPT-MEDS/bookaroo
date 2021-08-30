e4a95266a08e13d8a063a1088abe6590b6202dca
Giraugh <s3845382@student.rmit.edu.au>
2021-08-30 14:39:50 +1000

    Update books service to connect to database

--- frontend/src/pages/BookPage/BookPage.js
+++ frontend/src/pages/BookPage/BookPage.js
@@ -6,10 +6,12 @@ import { getAllBooks } from '../../services'
 const BookPage = () => {
   const [books, setBooks] = useState()
 
+  console.log(books)
+
   useEffect(() => {
     getAllBooks()
       .then(books => setBooks(books))
-  })
+  }, [])
 
   return <BookGrid books={books}/>
 }
--- frontend/src/services/book.js
+++ frontend/src/services/book.js
@@ -1,36 +1,28 @@
-//import api from './'
+import intersectionBy from 'lodash.intersectionby'
+import unionBy from 'lodash.unionby'
+import api from './'
 
-// TODO: remove
-const testBooks = [
-  {
-    title: 'My Cool Book',
-    author: 'Ewan Breakey',
-    rating: 4
-  },
-  {
-    title: 'Another Great Book',
-    author: 'Maxwell Reid',
-    rating: 1
-  },
-  {
-    title: 'Something I Found On The Floor',
-    author: 'Thomas Dib',
-    rating: 3
-  },
-  {
-    title: 'Nuclear War: A Threat to Us All',
-    author: 'Sefanur Erciyas',
-    rating: 5
-  },
-  {
-    title: 'The Weather Today: An Intro to C',
-    author: 'Dale Stanbrough',
-    rating: 5,
+export const getAllBooks = async (filter, category) => {
+  try {
+    const {data: {books: allBooks}} = await (category
+      ? api.get(`/book/byCategory/${category}`)
+      : api.get('/book'))
+
+    let filteredBooks = allBooks
+    if (filter) {
+      const filtered = (await Promise.all([
+        api.get(`/book/containingTitle/${filter}`),
+        api.get(`/book/containingAuthor/${filter}`),
+      ])).map(r => r.data.books)
+      filteredBooks = unionBy(filtered, b => b.isbn)
+    }
+
+    console.log(filteredBooks)
+    console.log(allBooks)
+
+    return intersectionBy([...allBooks, ...filteredBooks], b => b.isbn)
+  } catch (e) {
+    console.warn(e)
+    return []
   }
-]
-
-export const getAllBooks = async filter => {
-  console.log('Should have hit the api using', filter)
-  const {data} = {data: testBooks} // TODO: await api.get('/book', { filter })
-  return data
 }
