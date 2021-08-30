8fd338180b6f8027aeef84ff619d6345f9a6079b
Giraugh <s3845382@student.rmit.edu.au>
2021-08-30 16:13:07 +1000

    Extract the Notification element from login form

---
+++ frontend/src/components/Notification/notificationStyle.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding: 1em;
+  width: 100%;
+  color: ${p => p.theme.error};
+  background: ${p => p.theme.background};
+  border-radius: .5rem;
+  border: 2px solid currentColor;
+
+  &.error {
+    color: ${p => p.theme.error};
+    background: ${p => p.theme.errorBackground};
+  }
+`
