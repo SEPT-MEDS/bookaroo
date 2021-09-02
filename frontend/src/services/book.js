54234cad170ffe4967e83f7068cf9b78d221c3d3
Giraugh <s3845382@student.rmit.edu.au>
2021-09-02 17:57:11 +1000

    Fix filtering logic in getAllBooks service method

--- frontend/src/services/book.js
+++ frontend/src/services/book.js
@@ -1,5 +1,5 @@
 import intersectionBy from 'lodash.intersectionby'
-import unionBy from 'lodash.unionby'
+//import unionBy from 'lodash.unionby'
 import api from './'
 
 export const getBook = async isbn => {
@@ -13,16 +13,15 @@ export const getAllBooks = async (filter, category) => {
       ? api.get(`/book/byCategory/${category}`)
       : api.get('/book'))
 
-    let filteredBooks = allBooks
-    if (filter) {
-      const filtered = (await Promise.all([
-        api.get(`/book/containingTitle/${filter}`),
-        api.get(`/book/containingAuthor/${filter}`),
-      ])).map(r => r.data.books)
-      filteredBooks = unionBy(filtered, b => b.isbn)
-    }
+    if (!filter)
+      return allBooks
 
-    return intersectionBy([...allBooks, ...filteredBooks], b => b.isbn)
+    const filteredBooks = (await Promise.all([
+      api.get(`/book/containingTitle/${filter}`),
+      api.get(`/book/containingAuthor/${filter}`),
+    ])).map(r => r.data.books).reduce((a, b) => [...a, ...b], [])
+
+    return intersectionBy(allBooks, filteredBooks, 'isbn')
   } catch (e) {
     console.warn(e)
     return []
