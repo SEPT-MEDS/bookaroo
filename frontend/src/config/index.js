35a8e544bbd80b583fca1553b8a30c3c74f23238
Maxwell Reid s3787033 <s3787033@student.rmit.edu.au>
2021-09-04 04:59:25 +1000

    Change docker hub image tag

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,7 +1,11 @@
-const env = 'development'
+const env = window.process.env.NODE_ENV
 
 const config = {
   development: {
+    baseURL: 'http://localhost:3000',
+    apiAddress: 'http://localhost:9191/api',
+  },
+  production: {
     baseURL: 'http://localhost:3000',
     apiAddress: 'http://localhost:9191/api',
   }
