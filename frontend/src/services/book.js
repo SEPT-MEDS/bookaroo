9345bf8ea48c511eb5d7f220bd6b7023d93fda46
Giraugh <s3845382@student.rmit.edu.au>
2021-09-05 02:35:27 +1000

    Search by ISBN

--- frontend/src/services/book.js
+++ frontend/src/services/book.js
@@ -16,12 +16,17 @@ export const getAllBooks = async (filter, category) => {
     if (!filter)
       return allBooks
 
+    // Search books by title and author
     const filteredBooks = (await Promise.all([
       api.get(`/book/containingTitle/${filter}`),
       api.get(`/book/containingAuthor/${filter}`),
     ])).map(r => r.data.books).reduce((a, b) => [...a, ...b], [])
 
-    return intersectionBy(allBooks, filteredBooks, 'isbn')
+    // Search books by ISBN
+    const exactBook = await getBook(filter)
+      .catch(() => {console.clear() /* HACK*/})
+
+    return intersectionBy(allBooks, [...filteredBooks, exactBook], 'isbn')
   } catch (e) {
     console.warn(e)
     return []
