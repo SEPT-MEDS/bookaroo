a5f3c7172f520f1e7723ce48af61a8202ece8a31
Maxwell Reid s3787033 <s3787033@student.rmit.edu.au>
2021-09-06 04:48:43 +1000

    update API url in axios

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -4,6 +4,10 @@ const config = {
   development: {
     baseURL: 'http://localhost:3000',
     apiAddress: 'http://localhost:9191/api',
+  },
+  production: {
+    baseURL: 'https://develop.d1byy9xadzuk7b.amplifyapp.com',
+    apiAddress: 'http://13.238.218.243:9191/api'
   }
 }
 
