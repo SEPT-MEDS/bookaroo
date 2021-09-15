07d73b6a54d2ac1bb09a932019655c2d42f40010
Sefanur Erciyas s3842307 <s3842307@student.rmit.edu.au>
2021-09-15 17:54:37 +1000

    Extract BookInfo component from BookDetailPage

---
+++ frontend/src/components/BookInfo/BookInfo.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { BookCover, Rating } from 'components'
+import {
+  BookInfoContainer,
+  BookInfoPara,
+  BookInfoAuthor,
+  BookInfoDetails
+} from './bookInfoStyle'
+
+const BookInfo = ({ book }) => {
+  return (
+    <BookInfoContainer>
+      <div>
+        <BookCover isbn={book.isbn} />
+        <h1>{book.title}</h1>
+        <BookInfoAuthor>{book.author}</BookInfoAuthor>
+        <Rating rating={book.rating} />
+      </div>
+      <div>
+        <h2>Blurb</h2>
+        <BookInfoPara>{book.blurb || <em>No Blurb Provided</em>}</BookInfoPara>
+        <BookInfoDetails>
+          <div>
+            <h2>Number of Pages</h2>
+            <BookInfoPara>{book.numPages}</BookInfoPara>
+          </div>
+          <div>
+            <h2>Category</h2>
+            <BookInfoPara>{book.category || <em>No Category</em>}</BookInfoPara>
+          </div>
+        </BookInfoDetails>
+      </div>
+    </BookInfoContainer>
+  )
+}
+
+export default BookInfo
--- frontend/src/pages/BookDetailPage/BookDetailPage.js
+++ frontend/src/pages/BookDetailPage/BookDetailPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-import { BookCover, Rating, Spinner, Notification } from 'components'
+import { Spinner, Notification, BookInfo } from 'components'
 import { getBook } from 'services'
 import { useAsync } from 'hooks'
 
@@ -9,10 +9,6 @@ import BookSellers from './BookSellers'
 
 import {
   Container,
-  BookInfoContainer,
-  BookInfoPara,
-  BookInfoAuthor,
-  BookInfoDetails
 } from './bookDetailPageStyle'
 
 const BookDetailPage = () => {
@@ -34,31 +30,5 @@ const BookDetailPage = () => {
   )
 }
 
-const BookInfo = ({ book }) => {
-  return (
-    <BookInfoContainer>
-      <div>
-        <BookCover isbn={book.isbn} />
-        <h1>{book.title}</h1>
-        <BookInfoAuthor>{book.author}</BookInfoAuthor>
-        <Rating rating={book.rating} />
-      </div>
-      <div>
-        <h2>Blurb</h2>
-        <BookInfoPara>{book.blurb || <em>No Blurb Provided</em>}</BookInfoPara>
-        <BookInfoDetails>
-          <div>
-            <h2>Number of Pages</h2>
-            <BookInfoPara>{book.numPages}</BookInfoPara>
-          </div>
-          <div>
-            <h2>Category</h2>
-            <BookInfoPara>{book.category || <em>No Category</em>}</BookInfoPara>
-          </div>
-        </BookInfoDetails>
-      </div>
-    </BookInfoContainer>
-  )
-}
 
 export default BookDetailPage
