b636df3cf5bab5bb87f1b7acd85cf160e87beba9
Thomas Dib s3838765 <s3838765@student.rmit.edu.au>
2021-09-21 04:37:19 +1000

    Begin PayPal payment implementation

--- frontend/src/pages/ListingDetailPage/ListingDetailPage.js
+++ frontend/src/pages/ListingDetailPage/ListingDetailPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { useParams } from 'react-router-dom'
 
 import { useAsync } from 'hooks'
 import { getListing, getBook, getUser } from 'services'
-import { Notification, Spinner, BookSummary, Rating, Button } from 'components'
+import { Notification, Spinner, BookSummary, Rating/*, Button*/} from 'components'
 import { Container, ListingInfoContainer, ActionBox } from './listingDetailPageStyle'
+
+const PayPalButton = window.paypal.Buttons.driver('react', { React, ReactDOM })
 
 const ListingDetailPage = () => {
   const { id } = useParams()
@@ -30,7 +33,56 @@ const ListingInfo = ({ id, sellerId, price, isSwap, imageUrl, isPreowned, bookIs
 
   const handleAddToCart = () => {
     // TODO
+    
+    // const createOrder = (data, actions) => {
+    //   return actions.order.create({
+    //     purchase_units: [
+    //       {
+    //         amount: {
+    //           value: '15.30'
+    //         }
+    //       }
+    //     ]
+    //   })
+    // }
+    
+    // const onApprove = (data, actions) => {
+    //   console.log('ORDER COMPLETE')
+    //   return actions.order.capture()
+    // }
+    
+    
     console.log('Add listing', id, 'to cart')
+    
+    // return (
+    //   <PayPalButton
+    //     createOrder={(data, actions) => createOrder(data, actions)}
+    //     onApprove={(data, actions) => onApprove(data, actions)}
+    //   />
+    // )
+
+    // <script src="https://www.paypal.com/sdk/js?client-id=AZ3bA7tCRrgjEKtw8kKPnOBP6TY3kVdLLo0pkuVWWSgTUGAZdEUf1mCbl99eKk1fJcS7P3RyffFs9Eei"></script>
+    // <script>paypal.Buttons().render('body');</script>
+  }
+  console.log(handleAddToCart)
+
+
+
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            // Order total (use variable for price/cart total)
+            value: '15.30'
+          }
+        }
+      ]
+    })
+  }
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture()
   }
 
   return <ListingInfoContainer>
@@ -42,7 +94,12 @@ const ListingInfo = ({ id, sellerId, price, isSwap, imageUrl, isPreowned, bookIs
       <ActionBox>
         {!isSwap && <h3>Buy {isPreowned ? 'preowned' : 'brand new'} for ${ price }</h3>}
         {isSwap && <h3>Swap with <em>{vendor?.username}</em> for another book</h3>}
-        <Button onClick={handleAddToCart}>{isSwap ? `Contact ${vendor?.username || 'seller'}` : 'Buy Now'}</Button>
+        <PayPalButton
+          createOrder={(data, actions) => createOrder(data, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+
+        {/* <Button onClick={handleAddToCart}>{isSwap ? `Contact ${vendor?.username || 'seller'}` : 'Buy Now'}</Button> */}
       </ActionBox>
     </div>
   </ListingInfoContainer>
