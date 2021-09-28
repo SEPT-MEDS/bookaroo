975afb6cf7e777d0a43f0b36deb0bc48a597a74f
Sefanur Erciyas s3842307 <s3842307@student.rmit.edu.au>
2021-09-28 18:04:43 +1000

    Add params to BookSummary for hiding content

--- frontend/src/components/BookSummary/BookSummary.js
+++ frontend/src/components/BookSummary/BookSummary.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import {BookCover, Rating} from 'components'
 
 import {Author} from './bookSummaryStyle'
 
-const BookSummary = ({ book, imageUrl }) => <div>
-  <BookCover isbn={book?.isbn} imageUrl={imageUrl} />
-  <h1>{book?.title || 'Book'}</h1>
+const BookSummary = ({ book, imageUrl, showCover=true, showLink=false }) => <div>
+  {showCover && <BookCover isbn={book?.isbn} imageUrl={imageUrl} />}
+  <h1>
+    {showLink && <Link to={`/book/${book?.isbn}`}>{book?.title || 'Book'}</Link> }
+    {!showLink && (book?.title || 'Book')}
+  </h1>
   <Author>{book?.author || 'Author'}</Author>
   <Rating rating={book?.rating || 0} />
 </div>
