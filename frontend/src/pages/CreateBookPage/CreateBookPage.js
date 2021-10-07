6889d6f0f2955e125b248cc88530f3c01722f8bb
Thomas Dib s3838765 <s3838765@student.rmit.edu.au>
2021-10-08 00:15:44 +1100

    Implement admin adding books

--- frontend/src/pages/CreateBookPage/CreateBookPage.js
+++ frontend/src/pages/CreateBookPage/CreateBookPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 
-import { useAsync } from 'hooks'
+import { useAsync, useCurrentProfile } from 'hooks'
 import { createBook, getOLBookDetails } from 'services'
 import { Spinner, BookCover } from 'components'
 import {
@@ -24,6 +24,7 @@ const CreateBookPage = () => {
   const { isbn } = useParams()
   const { register, handleSubmit, setValue } = useForm()
   const { response: apiRes } = useAsync(() => getOLBookDetails(isbn))
+  const profile = useCurrentProfile()
 
   useEffect(() => {
     if (apiRes) {
@@ -45,7 +46,12 @@ const CreateBookPage = () => {
       title,
       category,
       url: 'null',
-    }).then(() => history.push(`/listing/new/${isbn}`))
+      rating: 0
+    }).then(() => {
+      profile.type === 'ADMIN'
+        ? history.push('/')
+        : history.push(`/listing/new/${isbn}`)
+    })
   }
 
   return (
