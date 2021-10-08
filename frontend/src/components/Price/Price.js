7fe898bd91f260ee06818ae2d4dabb2047263b51
Thomas Dib s3838765 <s3838765@student.rmit.edu.au>
2021-10-08 19:08:02 +1100

    Add closing tag on Price component

--- frontend/src/components/Price/Price.js
+++ frontend/src/components/Price/Price.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Price = (price, currency = 'AUD') => {
   return <>
     {Number(price).toLocaleString(undefined, { style: 'currency', currency })}
-  <>
+  </>
 }
 
 export default Price
