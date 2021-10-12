062bf039dac0d73a0e2d1f45dc19f043a83e730c
Thomas Dib s3838765 <s3838765@student.rmit.edu.au>
2021-10-12 15:42:33 +1100

    Add link from book listing to book page

--- frontend/src/components/BookSummary/bookSummaryStyle.js
+++ frontend/src/components/BookSummary/bookSummaryStyle.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 export const Author = styled.h3`
   color: ${p => p.theme.greyText};
+`
+
+export const Title = styled(Link)`
+  text-decoration: none;
+  color: ${p => p.theme.primary};
+  font-size: 1.5rem;
+  font-weight: bold;
 `
