fc2fc1f40541e8aae194751df9f57f17e0237b77
Giraugh <ebreakey@gmail.com>
2021-10-22 16:01:02 +1100

    Write test spec for Logo component

---
+++ frontend/src/components/Logo/Logo.test.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import Logo from './Logo'
+
+it('Renders the site name', () => {
+  render(<Logo />)
+  expect(screen.getByText('Bookaroo')).toBeTruthy()
+})
+
+it('Renders a heading', () => {
+  render(<Logo />)
+  expect(screen.getByRole('heading')).toBeTruthy()
+})
+
+it('Renders a styled heading', () => {
+  render(<Logo />)
+  expect(screen.getByRole('heading').className).toBeTruthy()
+})
+
