0430ff3fe1f92a618adf03d56efdc62de527499f
Giraugh <ebreakey@gmail.com>
2021-10-22 16:31:15 +1100

    Write test spec for Spinner component

---
+++ frontend/src/components/Spinner/Spinner.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import Spinner from './Spinner'
+
+it('Renders a styled component', () => {
+  const { container } = render(<Spinner />)
+  expect(container.firstChild?.className).toBeTruthy() 
+})
+
