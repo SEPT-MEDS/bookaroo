6d031e1af550f6407e3c6ddd02da9f3ad2bcbbfe
Giraugh <ebreakey@gmail.com>
2021-10-23 14:28:08 +1100

    Fix formatting of test for Button component

--- frontend/src/components/Button/Button.test.js
+++ frontend/src/components/Button/Button.test.js
@@ -3,6 +3,7 @@ import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Button from './Button'
 
+
 it('Renders a button', () => {
   render(<Button />)
   expect(screen.getByRole('button')).toBeTruthy()
