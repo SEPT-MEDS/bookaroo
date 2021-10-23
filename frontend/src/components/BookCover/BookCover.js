c7162a561d84e2a68aa5d78aa680e2c0814f4aee
Giraugh <ebreakey@gmail.com>
2021-10-23 14:15:06 +1100

    Neaten internal documentation in BookCover

--- frontend/src/components/BookCover/BookCover.js
+++ frontend/src/components/BookCover/BookCover.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import { Cover } from './bookCoverStyle'
 
-// Image of a given book, used wherever an image is required (e.g. main book page, book edit page, etc)
+// Image of a given book, used wherever an image is required
+// (e.g. main book page, book edit page, etc)
 const BookCover = ({ isbn, imageUrl }) => {
-  // Uses passed in image URL if available, otherwise attempt to use OpenLibrary API image
+  // Uses passed in image URL if available,
+  // otherwise attempt to use OpenLibrary API image
   return <Cover style={{ backgroundImage: imageUrl ? `url(${imageUrl})` : `url(https://covers.openlibrary.org/b/isbn/${isbn}-L.jpg)` }} />
 }
 
